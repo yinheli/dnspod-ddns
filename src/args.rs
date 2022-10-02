7092ecd5b1b8c0684cd1f5f8c163d11b9cf0be19
yinheli <me@yinheli.com>
2022-10-02 09:15:05 +0800

    chore(deps): upgrade clap and others

--- src/args.rs
+++ src/args.rs
@@ -2,28 +2,28 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[clap(author, version, about)]
+#[command(author, version, about)]
 pub struct Args {
     /// domain, eg: example.com
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub domain: String,
 
     /// sub domain, eg: www
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub sub_domain: String,
 
     /// dnspod api key / token, eg: 12345,aeaae98e8fbee8369f93ec46c4384aed
-    #[clap(short, long, env = "DNSPOD_API_KEY")]
+    #[arg(short, long, env = "DNSPOD_API_KEY")]
     pub token: String,
 
     /// check interval seconds, eg: 10
     /// default: 10
     /// min: 5
     /// max: 3600
-    #[clap(short, long, env = "DNSPOD_INTERVAL", default_value = "10", value_parser = clap::value_parser!(u64).range(5..=3600))]
+    #[arg(short, long, env = "DNSPOD_INTERVAL", default_value = "10", value_parser = clap::value_parser!(u64).range(5..=3600))]
     pub interval: u64,
 
     /// verbose log
-    #[clap(long)]
+    #[arg(long)]
     pub verbose: bool,
 }
